e4602768e48c1a54ba9fc767dfa84932d510a032
ChristianDelvianto <github.placeholder.delvin@gmail.com>
2026-02-20 02:51:54 +0700

    add API middleware for role checking and unauthenticated response

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +13,20 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->api(
+            [],
+            [
+                \App\Http\Middleware\ForceJsonResponseMiddleware::class,
+            ],
+            [],
+            [],
+        )->alias([
+            'role' => \App\Http\Middleware\RoleCheckMiddleware::class,
+        ])->redirectGuestsTo(function (Request $request) {
+            return response()->json([
+                'message' => 'Unauthenticated.'
+            ], 401);
+        });
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
