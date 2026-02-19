b5e2959f6149862dde645137f618f5205f5de0e2
ChristianDelvianto <github.placeholder.delvin@gmail.com>
2026-02-20 02:53:26 +0700

    add PlanSeeder

---
+++ database/seeders/PlanSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Plan;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PlanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Free
+        Plan::create(['price' => 0, 'type' => 'free', 'size' => 0]);
+
+
+    }
+}
