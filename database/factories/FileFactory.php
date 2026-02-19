585cf2b70e7df737fbe4d0b7a860236855c5636f
ChristianDelvianto <github.placeholder.delvin@gmail.com>
2026-02-20 02:36:38 +0700

    fill FileFactory

--- database/factories/FileFactory.php
+++ database/factories/FileFactory.php
@@ -16,8 +16,29 @@ class FileFactory extends Factory
      */
     public function definition(): array
     {
+        $mime = fake()->randomElement(['image/jpg', 'image/jpeg', 'image/webp', 'video/mp4', 'video/webm', 'audio/mp3']);
+
+        $category = explode('/', $mime)[0];
+        $extension = explode('/', $mime)[1];
+
         return [
-            //
+            'category' => $category,
+            'mime_type' => $mime,
+            'name' => fake()->words(6, true) . ".{$extension}",
+            'size' => fake()->numberBetween(5 * 1024),
+            'thumbnail_path' => null,
+            'storage_path' => 'somewhere_to_put',
+            'disk' => fake()->randomElement(['s3', 'local', 'r2', 'gdrive']),
         ];
+    }
+
+    /**
+     * Indicate that the model should be deleted
+     */
+    public function deleted(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'deleted_at' => now(),
+        ]);
     }
 }
