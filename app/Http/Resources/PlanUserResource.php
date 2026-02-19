e050c20e4fe4c0e6c05f283b47f9852639df87f0
ChristianDelvianto <github.placeholder.delvin@gmail.com>
2026-02-20 01:58:22 +0700

    add PlanResource, PlanUserResource and UserResource

---
+++ app/Http/Resources/PlanUserResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PlanUserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->plan->id, // Easy to check on frontend
+            'size' => $this->plan->size,
+            'type' => $this->plan->type,
+        ];
+    }
+}
