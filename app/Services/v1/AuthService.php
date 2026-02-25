178c9fa09060b73e210b59b3f48faec1d6b1b313
ChristianDelvianto <github.placeholder.delvin@gmail.com>
2026-02-25 19:50:44 +0700

    refactor AuthService and FileService to enhance user registration and file management; implement transaction handling and caching for file operations

--- app/Services/v1/AuthService.php
+++ app/Services/v1/AuthService.php
@@ -2,17 +2,55 @@
 
 namespace App\Services\v1;
 
+use App\Http\Resources\PlanUserResource;
+use App\Http\Resources\UserResource;
+use App\Models\Plan;
+use App\Models\PlanUser;
 use App\Models\User;
 use DateTimeInterface;
+use Exception;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 class AuthService
 {
+    /**
+     * Generate a new access token with the right credentials
+     */
     public function generateNewToken(User $user, ?array $abilities = ['*'], ?DateTimeInterface $expiresAt = null): string
     {
         $tokenName = Str::random(config('sanctum.token_name_length'));
         $token = $user->createToken($tokenName, $abilities, $expiresAt);
 
         return $token->plainTextToken;
+    }
+
+    /**
+     * Store new user and assign free plan, then generate access token
+     */
+    public function registerNewUser(array $userData): array
+    {
+        try {
+            return DB::transaction(function () use ($userData): array {
+                $freePlan = Plan::firstWhere('price', 0);
+
+                $user = User::create(['role' => 'user', ...$userData]);
+
+                $planUser = PlanUser::create(['plan_id' => $freePlan->id, 'user_id' => $user->id]);
+                $planUser->setRelation('plan', $freePlan);
+
+                $plainToken = $this->generateNewToken($user);
+
+                // Send verification email
+
+                return [
+                    'plan' => PlanUserResource::make($planUser),
+                    'profile' => UserResource::make($user),
+                    'token' => $plainToken,
+                ];
+            });
+        } catch (Exception $e) {
+            throw new Exception('Internal server error.', 500);
+        }
     }
 }
--- app/Services/v1/FileService.php
+++ app/Services/v1/FileService.php
@@ -2,11 +2,20 @@
 
 namespace App\Services\v1;
 
+use App\Models\File;
+use App\Models\User;
+use Exception;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class FileService
 {
+    /**
+     * Get file category
+     */
     public function getCategory(UploadedFile $file): string
     {
         $mimeType = $file->getMimeType();
@@ -22,9 +31,106 @@ class FileService
         } elseif (in_array($mimeType, $documentMimes)) {
             $category = 'document';
         } else {
-            $category = 'others';
+            $category = 'other';
         }
 
         return $category;
+    }
+
+    /**
+     * Soft delete file and update user used disk
+     */
+    public function softDeleteFile(File $file): void
+    {
+        $lock = Cache::lock("file:{$file->id}:update", 10);
+
+        try {
+            $lock->get();
+
+            DB::transaction(function () use ($file) {
+                $file->delete();
+            });
+        } catch (Exception $e) {
+            throw new Exception('Internal server error.', 500);
+        } finally {
+            $lock?->release();
+        }
+    }
+
+    /**
+     * Store file and create new table record
+     */
+    public function storeFileAndCreateRecord(User $user, UploadedFile $uploadedFile, null|UploadedFile $thumbnail = null): File
+    {
+        $lock = Cache::lock("user:{$user->id}:update", 10);
+
+        try {
+            $lock->get();
+
+            return DB::transaction(function () use ($thumbnail, $uploadedFile, $user) {
+                $uuid = Str::uuid()->toString();
+                $mimeType = $uploadedFile->getMimeType();
+                $fileName = $uploadedFile->getClientOriginalName();
+                $fileExtension = explode('/', $mimeType)[1];
+                $filePath = "{$fileName}.{$fileExtension}";
+                $fileCategory = $this->getCategory($uploadedFile);
+                $thumbnailPath = null;
+
+                Storage::disk('public')->putFileAs($uuid, $uploadedFile, $filePath);
+
+                if ($fileCategory === 'video') {
+                    $thumbnailPath = 'thumbnail_' . Str::random() . '.jpeg';
+
+                    Storage::disk('public')->putFileAs($uuid, $thumbnail, $thumbnailPath);
+                }
+
+                $file = File::create([
+                            'uuid' => $uuid,
+                            'category' => $fileCategory,
+                            'extension' => $fileExtension,
+                            'mime_type' => $mimeType,
+                            'name' => $fileName . '.' . $fileExtension,
+                            'size' => $uploadedFile->getSize(),
+                            'thumbnail_path' => $thumbnailPath,
+                            'storage_path' => $filePath,
+                            'disk' => config('filesystems.default'),
+                            'user_id' => $user->id,
+                        ]);
+
+                $user->increment('used_disk', $uploadedFile->getSize());
+
+                return $file;
+            });
+        } catch (Exception $e) {
+            throw new Exception('Internal server error.', 500);
+        } finally {
+            $lock?->release();
+        }
+    }
+
+    /**
+     * Update file table record
+     */
+    public function updateFileRecord(File $file, array $data): File
+    {
+        $lock = Cache::lock("file:{$file->id}:update", 10);
+
+        try {
+            $lock->get();
+
+            return DB::transaction(function () use ($file, $data) {
+                $file->fill($data);
+
+                if ($file->isDirty()) {
+                    $file->save();
+                }
+
+                return $file;
+            });
+        } catch (Exception $e) {
+            throw new Exception('Internal server error.', 500);
+        } finally {
+            $lock?->release();
+        }
     }
 }
