a9165da762a3a5542969cdd0002c6a850db3aaca
ChristianDelvianto <github.placeholder.delvin@gmail.com>
2026-04-06 14:32:36 +0700

    refactor users migration; change role column to enum type and update indexes for clarity

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,17 +13,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('role')->default('user'); // admin, user
+            $table->enum('role', ['admin', 'user'])->default('user');
             $table->unsignedBigInteger('used_bytes')->default(0);
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
             // $table->rememberToken();
+            $table->timestamp('delete_all_at')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
 
             // Indexes
-            $table->index('created_at');
+            $table->index(['role', 'created_at']);
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
