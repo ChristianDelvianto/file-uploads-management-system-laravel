dc7df269593982aeabef94585c1c97e9316a01a4
ChristianDelvianto <github.placeholder.delvin@gmail.com>
2026-04-06 15:07:51 +0700

    rename user recent route, delete-all functionality, and logs endpoint to user API

--- routes/api/v1/user.php
+++ routes/api/v1/user.php
@@ -7,11 +7,14 @@ Route::prefix('user')
 ->as('user.')
 ->middleware(['auth:sanctum', 'role:user'])
 ->group(function () {
-    Route::get('recent', [UserDashboardController::class, 'recent'])->name('recent');
+    Route::get('home', [UserDashboardController::class, 'home'])->name('home');
     Route::get('images', [UserDashboardController::class, 'images'])->name('images');
     Route::get('videos', [UserDashboardController::class, 'videos'])->name('videos');
     Route::get('audios', [UserDashboardController::class, 'audios'])->name('audios');
     Route::get('documents', [UserDashboardController::class, 'documents'])->name('documents');
     Route::get('others', [UserDashboardController::class, 'others'])->name('others');
     Route::get('deleted', [UserDashboardController::class, 'deleted'])->name('deleted');
+    Route::delete('delete-all', [UserDashboardController::class, 'deleteAll'])->name('delete-all');
+
+    Route::get('logs', [UserDashboardController::class, 'logs'])->name('logs');
 });
