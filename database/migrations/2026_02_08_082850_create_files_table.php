871a84ebc2ea223fe5efa03aebb28c3ef478cbed
ChristianDelvianto <github.placeholder.delvin@gmail.com>
2026-04-06 14:33:14 +0700

    refactor files migration; add file visibility and update index

--- database/migrations/2026_02_08_082850_create_files_table.php
+++ database/migrations/2026_02_08_082850_create_files_table.php
@@ -15,17 +15,21 @@ return new class extends Migration
         Schema::create('files', function (Blueprint $table) {
             $table->id();
             $table->uuid('uuid')->unique();
-            
+
+            $table->enum('visibility', ['private', 'public', 'shared'])->default('private');
+
+            // $table->boolean('is_scanned')->default(false);
+
             // Metadata
             $table->string('category', 20);
             $table->string('extension', 40);
             $table->string('mime_type', 100);
-            $table->string('name');
-            $table->unsignedBigInteger('size', false);
+            $table->string('name'); // Original file name
+            $table->unsignedBigInteger('bytes_size', false);
             
-            $table->string('thumbnail_path')->nullable(); // For video
-            $table->string('storage_path');
-            $table->string('disk', 20); // s3, r2, supabase, gdrive
+            $table->string('thumbnail_name')->nullable(); // For video
+            $table->string('storage_name');
+            $table->string('disk', 20); // s3, r2, supabase, gdrive, etc
             $table->foreignId('user_id')->constrained('users', 'id')->cascadeOnDelete();
             $table->timestamps();
             $table->softDeletes();
@@ -33,14 +37,13 @@ return new class extends Migration
             // Indexes
 
             // User-Pattern
-            $table->index(['user_id', 'deleted_at', 'updated_at']);
-            $table->index(['user_id', 'category', 'deleted_at', 'updated_at']);
-            $table->index(['user_id', 'name', 'deleted_at', 'updated_at']);
+            $table->index(['user_id', 'category', 'deleted_at', 'created_at']);
+            $table->index(['user_id', 'name', 'category', 'deleted_at', 'created_at']);
+            $table->index(['user_id', 'deleted_at']);
 
             // Admin-Pattern (Global)
-            $table->index(['deleted_at', 'updated_at']);
-            $table->index(['size', 'deleted_at']);
-            // $table->index(['category', 'deleted_at', 'updated_at']);
+            $table->index(['deleted_at', 'created_at']);
+            $table->index(['bytes_size', 'deleted_at']);
         });
 
         // Full-Text index
