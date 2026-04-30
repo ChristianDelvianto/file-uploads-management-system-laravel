f7b6c316639ed63aedc70debae4b6c4e9159fb22
ChristianDelvianto <christiandelvianto1@gmail.com>
2026-04-30 07:41:34 +0700

    refactor AppServiceProvider and update API routes for rate limiting and file handling

--- routes/api/v1/index.php
+++ routes/api/v1/index.php
@@ -4,8 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')
 ->as('v1.')
+->middleware(['throttle:api'])
 ->group(function () {
     require __DIR__ . '/auth.php';
-    require __DIR__ . '/file.php';
+    require __DIR__ . '/files.php';
+    require __DIR__ . '/uploads.php';
     require __DIR__ . '/user.php';
 });
