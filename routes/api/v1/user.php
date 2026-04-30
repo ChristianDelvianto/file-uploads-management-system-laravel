a0c0637bc3f33808837593edefbcac3fc969d3b0
ChristianDelvianto <christiandelvianto1@gmail.com>
2026-04-30 07:44:00 +0700

    refactor user routes: remove unused endpoints and organize file management routes

--- routes/api/v1/user.php
+++ routes/api/v1/user.php
@@ -7,14 +7,18 @@ Route::prefix('user')
 ->as('user.')
 ->middleware(['auth:sanctum', 'role:user'])
 ->group(function () {
-    Route::get('home', [UserDashboardController::class, 'home'])->name('home');
-    Route::get('images', [UserDashboardController::class, 'images'])->name('images');
-    Route::get('videos', [UserDashboardController::class, 'videos'])->name('videos');
-    Route::get('audios', [UserDashboardController::class, 'audios'])->name('audios');
-    Route::get('documents', [UserDashboardController::class, 'documents'])->name('documents');
-    Route::get('others', [UserDashboardController::class, 'others'])->name('others');
-    Route::get('deleted', [UserDashboardController::class, 'deleted'])->name('deleted');
-    Route::delete('delete-all', [UserDashboardController::class, 'deleteAll'])->name('delete-all');
+    Route::get('activities', [UserDashboardController::class, 'activities'])
+    ->name('activities');
 
-    Route::get('logs', [UserDashboardController::class, 'logs'])->name('logs');
+    Route::get('files', [UserDashboardController::class, 'files'])
+    ->name('files');
+
+    Route::prefix('trash')
+    ->group(function () {
+        Route::delete('/', [UserDashboardController::class, 'deleteTrashed'])
+        ->name('trashed.delete');
+
+        Route::get('/', [UserDashboardController::class, 'trashed'])
+        ->name('trashed');
+    });
 });
