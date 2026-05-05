69004062756b27c90cb9404d0a7d6db7c60baa61
ChristianDelvianto <christiandelvianto1@gmail.com>
2026-05-05 17:57:18 +0700

    improve UserFactory: add used_bytes and last_deleted_all_at

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,10 +25,12 @@ class UserFactory extends Factory
     {
         return [
             'role' => 'user',
+            'used_bytes' => 0,
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'password' => static::$password ??= Hash::make('password'),
             // 'remember_token' => Str::random(10),
+            'last_delete_all_at' => null,
             'email_verified_at' => now(),
         ];
     }
