40e076876f514f0f9e587dc215401fd71e8135c1
ChristianDelvianto <christiandelvianto1@gmail.com>
2026-05-19 08:52:06 +0700

    reduce API rate limit to 30 requests per minute and maintain upload limit at 100

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            return Limit::perMinute(30)->by($request->user()?->id ?: $request->ip());
+        });
+
+        RateLimiter::for('upload', function (Request $request) {
+            return Limit::perMinute(100)->by($request->user()->id);
         });
     }
 }
