eed05068b5bf1d6bca3c885df1f72f3b662df850
ChristianDelvianto <christiandelvianto1@gmail.com>
2026-06-21 10:41:31 +0700

    update PlanSeeder

--- database/seeders/PlanSeeder.php
+++ database/seeders/PlanSeeder.php
@@ -13,17 +13,13 @@ class PlanSeeder extends Seeder
      */
     public function run(): void
     {
-        // 1GB in bytes
-        // Plan::create(['name' => 'Free', 'price_cents' => 0, 'limit_bytes' => 1073741824]);
-
         // 5GB in bytes; $4.99
-        // Plan::create(['name' => 'Student', 'price_cents' => 499, 'limit_bytes' => 5368709120]);
-        Plan::create(['name' => 'Free', 'price_cents' => 0, 'limit_bytes' => 5368709120]);
+        Plan::create(['name' => 'Free', 'price' => 0, 'limit_bytes' => 5368709120]);
 
         // 20GB in bytes; $9.99
-        Plan::create(['name' => 'Pro', 'price_cents' => 999, 'limit_bytes' => 21474836480]);
+        Plan::create(['name' => 'Pro', 'price' => 999, 'limit_bytes' => 21474836480]);
 
         // 50GB in bytes; $19.99
-        Plan::create(['name' => 'Premium', 'price_cents' => 1999, 'limit_bytes' => 53687091200]);
+        Plan::create(['name' => 'Premium', 'price' => 1999, 'limit_bytes' => 53687091200]);
     }
 }
