4523ecc94ae5d8f12139616a117a9f8460450332
ChristianDelvianto <christiandelvianto1@gmail.com>
2026-06-21 11:27:10 +0700

    update Plan model; add eloquent relationships and update fillable array

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Plan extends Model
 {
@@ -17,9 +19,23 @@ class Plan extends Model
      */
     protected $fillable = [
         'name',
-        'price_cents',
-        'is_active',
+        'price',
         'limit_bytes',
     ];
+
+    /**
+     * The plan users that belong to the plan.
+     */
+    public function planUsers(): HasMany
+    {
+        return $this->hasMany(\App\Models\PlanUser::class, 'plan_id', 'id');
+    }
     
+    /**
+     * The users that belong to the plan.
+     */
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(\App\Models\User::class, 'plan_user', 'plan_id', 'user_id', 'id', 'id')->withTimestamps();
+    }
 }
