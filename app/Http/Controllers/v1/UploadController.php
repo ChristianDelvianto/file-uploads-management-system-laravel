ea56abf3fd2b5d110976d78db0d3c42c9f474600
ChristianDelvianto <christiandelvianto1@gmail.com>
2026-06-21 10:04:56 +0700

    add PHPDoc for UploadController

--- app/Http/Controllers/v1/UploadController.php
+++ app/Http/Controllers/v1/UploadController.php
@@ -8,8 +8,7 @@ use App\Http\Requests\v1\UploadStoreRequest;
 use App\Http\Requests\v1\UploadStoreChunkRequest;
 use App\Http\Resources\v1\UploadResource;
 use App\Policies\UploadPolicy;
-use App\Services\v1\UploadCompleteService;
-use App\Services\v1\UploadStoreService;
+use App\Services\v1\UploadService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
@@ -17,14 +16,16 @@ use Illuminate\Support\Facades\Gate;
 class UploadController extends Controller
 {
     public function __construct(
-        public UploadCompleteService $uploadCompleteService,
-        public UploadStoreService $uploadStoreService
+        protected UploadService $uploadService
     ) {
         // 
     }
 
     /**
      * Store a newly created resource in storage.
+     * 
+     * @param \App\Http\Requests\v1\UploadStoreRequest $request
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(UploadStoreRequest $request): JsonResponse
     {
@@ -32,7 +33,7 @@ class UploadController extends Controller
 
         Gate::forUser($user)->policy(Upload::class, UploadPolicy::class)->authorize('create', Upload::class);
 
-        $upload = $this->uploadStoreService->storeAndReturnModel($user, $request->except('thumbnail'), $request->file('thumbnail'));
+        $upload = $this->uploadService->store($user, $request->except('thumbnail'), $request->file('thumbnail'));
 
         return response()->json([
             'uuid' => $upload->uuid
@@ -40,21 +41,11 @@ class UploadController extends Controller
     }
 
     /**
-     * Store a chunk of the upload.
-     */
-    public function storeChunk(UploadStoreChunkRequest $request, Upload $upload): JsonResponse
-    {
-        $user = $request->user('sanctum');
-
-        Gate::forUser($user)->policy(Upload::class, UploadPolicy::class)->authorize('putFileChunk', $upload);
-
-        $this->uploadStoreService->saveChunk($upload, $request->validated('index'), $request->file('chunk'));
-
-        return response()->json(null, 204);
-    }
-
-    /**
      * Display the specified resource.
+     * 
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Upload $upload
+     * @return \Illuminate\Http\JsonResponse
      */
     public function show(Request $request, Upload $upload): JsonResponse
     {
@@ -68,7 +59,29 @@ class UploadController extends Controller
     }
 
     /**
+     * Store a chunk of the upload.
+     * 
+     * @param \App\Http\Requests\v1\UploadStoreChunkRequest $request
+     * @param \App\Models\Upload $upload
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function storeChunk(UploadStoreChunkRequest $request, Upload $upload): JsonResponse
+    {
+        $user = $request->user('sanctum');
+
+        Gate::forUser($user)->policy(Upload::class, UploadPolicy::class)->authorize('putChunk', $upload);
+
+        $this->uploadService->saveChunk($upload, $request->validated('index'), $request->file('chunk'));
+
+        return response()->json(null, 204);
+    }
+
+    /**
      * Cancel an upload.
+     * 
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Upload $upload
+     * @return \Illuminate\Http\JsonResponse
      */
     public function cancel(Request $request, Upload $upload): JsonResponse
     {
@@ -82,7 +95,11 @@ class UploadController extends Controller
     }
 
     /**
-     * Complete upload and return user's used_bytes.
+     * Complete upload and return user's `used_bytes`.
+     * 
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Upload $upload
+     * @return \Illuminate\Http\JsonResponse
      */
     public function complete(Request $request, Upload $upload): JsonResponse
     {
@@ -90,10 +107,10 @@ class UploadController extends Controller
 
         Gate::forUser($user)->policy(Upload::class, UploadPolicy::class)->authorize('complete', $upload);
 
-        $usedBytes = $this->uploadCompleteService->completeAndReturnUsedBytes($user, $upload);
+        $usedBytes = $this->uploadService->complete($user, $upload);
 
         return response()->json([
             'used_bytes' => $usedBytes
-        ], 201);
+        ]);
     }
 }
