a3a780aaae237e4d5d0f9b15fa7abccacb1740a5
ChristianDelvianto <christiandelvianto1@gmail.com>
2026-06-21 10:29:24 +0700

    refactor files table; add directory_path, change boolean is_scanned into enum-based column

--- database/migrations/2026_02_08_082850_create_files_table.php
+++ database/migrations/2026_02_08_082850_create_files_table.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -14,34 +13,32 @@ return new class extends Migration
     {
         Schema::create('files', function (Blueprint $table) {
             $table->id();
-            $table->uuid('uuid')->unique();
-            $table->enum('status', ['failed', 'processing', 'completed'])->default('completed');
+            $table->uuid('uuid');
+            $table->enum('status', ['completed', 'failed', 'processing'])->default('completed');
             $table->enum('visibility', ['private', 'public', 'shared'])->default('private');
-            $table->boolean('is_scanned')->default(false);
-            $table->string('disk', 20); // 'azure', 'gdrive', 'local', 'r2', 'supabase', 's3', etc
+            $table->enum('scan_status', ['clean', 'infected', 'none', 'pending'])->default('pending');
+            $table->string('disk', 100); // 'azure', 'gdrive', 'local', 'r2', 'supabase', 's3', etc
+            $table->string('directory_path');
 
             // Metadata
-            $table->string('category', 20);
+            $table->enum('category', ['audio', 'document', 'image', 'other', 'video']);
             $table->string('extension', 40);
             $table->string('mime_type', 100);
-            $table->string('name'); // Original file name - Slugged
+            $table->string('name'); // Original name
             $table->unsignedMediumInteger('duration')->nullable(); // Audio & video; In seconds
             $table->unsignedBigInteger('bytes_size');
-            
+
             $table->string('storage_name');
             $table->string('thumbnail_name')->nullable(); // For video
             $table->foreignId('user_id')->constrained('users', 'id')->cascadeOnDelete();
-            // $table->foreignId('folder_id')->nullable()->constrained('folders', 'id')->nullOnDelete();
             $table->timestamps();
             $table->softDeletes();
 
-            // Indexes
-            $table->index(['user_id', 'category', 'deleted_at', 'created_at']);
+            // Indexes (The indexes and composite indexes focuses on user-facing features only)
+            $table->unique('uuid');
+            $table->index(['user_id', 'category', 'created_at', 'deleted_at']);
             $table->index(['user_id', 'deleted_at']);
         });
-
-        // Full-Text index
-        // DB::statement('ALTER TABLE files ADD FULLTEXT fulltext_index (name)');
     }
 
     /**
