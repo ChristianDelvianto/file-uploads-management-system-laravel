263aa1d712042260044474cb67aa4703a47e2515
ChristianDelvianto <christiandelvianto1@gmail.com>
2026-06-21 09:53:35 +0700

    rename PruneTrashedFiles to ClearTrashed

--- app/Console/Commands/ClearTrashed.php
+++ app/Console/Commands/ClearTrashed.php
@@ -5,31 +5,31 @@ namespace App\Console\Commands;
 use App\Models\File;
 use Illuminate\Console\Command;
 
-class PruneTrashedFiles extends Command
+class ClearTrashed extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'app:prune-trashed-files';
+    protected $signature = 'app:clear-trashed';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Permanently delete trashed files.';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        $timestamp = now()->subDays(config('filesystems.prune_days_gap'));
+        $fromTimestamp = now()->subDays(config('filesystems.prune_days_gap'));
 
         File::onlyTrashed()
-        ->where('deleted_at', '<', $timestamp)
+        ->where('deleted_at', '<=', $fromTimestamp)
         ->orderBy('deleted_at', 'asc') // Start with the oldest
         ->chunkById(50, function ($files) {
             foreach ($files as $file) {
