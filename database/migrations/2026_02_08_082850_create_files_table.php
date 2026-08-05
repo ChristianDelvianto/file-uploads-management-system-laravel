7d9b931ff97283936b4b84614354edf1b95ae159
ChristianDelvianto <christiandelvianto1@gmail.com>
2026-08-05 18:31:48 +0700

    remove scan_status column from files table migration; update comments for clarity

--- database/migrations/2026_02_08_082850_create_files_table.php
+++ database/migrations/2026_02_08_082850_create_files_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->uuid('uuid');
             $table->enum('status', ['completed', 'failed', 'processing'])->default('completed');
             $table->enum('visibility', ['private', 'public', 'shared'])->default('private');
-            $table->enum('scan_status', ['clean', 'infected', 'none', 'pending'])->default('pending');
             $table->string('disk', 100); // 'azure', 'gdrive', 'local', 'r2', 'supabase', 's3', etc
             $table->string('directory_path');
 
@@ -24,20 +23,22 @@ return new class extends Migration
             $table->enum('category', ['audio', 'document', 'image', 'other', 'video']);
             $table->string('extension', 40);
             $table->string('mime_type', 100);
-            $table->string('name'); // Original name
+            $table->string('name'); // Original name (Editable)
             $table->unsignedMediumInteger('duration')->nullable(); // Audio & video; In seconds
             $table->unsignedBigInteger('bytes_size');
 
-            $table->string('storage_name');
+            $table->string('storage_name'); // Immutable
             $table->string('thumbnail_name')->nullable(); // For video
             $table->foreignId('user_id')->constrained('users', 'id')->cascadeOnDelete();
             $table->timestamps();
             $table->softDeletes();
 
-            // Indexes (The indexes and composite indexes focuses on user-facing features only)
+            // Indexes
             $table->unique('uuid');
-            $table->index(['user_id', 'category', 'created_at', 'deleted_at']);
-            $table->index(['user_id', 'deleted_at']);
+            $table->index(['user_id', 'deleted_at', 'created_at']); // User files
+            $table->index(['user_id', 'category', 'deleted_at', 'created_at']); // User specific files
+            $table->index(['user_id', 'bytes_size', 'deleted_at']); // User trash and delete all trashed
+            $table->index('deleted_at'); // For clean up cron job
         });
     }
 
