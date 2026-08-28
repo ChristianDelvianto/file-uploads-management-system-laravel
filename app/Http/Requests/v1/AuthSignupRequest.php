a768ddc3c706448aafb5b61044fb48a1cf85e3db
ChristianDelvianto <christiandelvianto1@gmail.com>
2026-08-28 16:47:44 +0700

    add AuthLoginRequest and AuthSignupRequest

---
+++ app/Http/Requests/v1/AuthSignupRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests\v1;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class AuthSignupRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'max:255',
+                'unique:users,email'
+            ],
+            'password' => [
+                'required',
+                'string',
+                'max:255',
+                'min:8',
+                'confirmed'
+            ],
+            'name' => [
+                'required',
+                'string',
+                'max:255',
+                'min:3'
+            ],
+        ];
+    }
+}
